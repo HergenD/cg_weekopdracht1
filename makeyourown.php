cd0d4295cb394f8c855fbc78749e90fb098a3ac8
n-esty <32772820+n-esty@users.noreply.github.com>
2018-11-10 20:10:48 +0100

    Add files via upload

--- makeyourown.php
+++ makeyourown.php
@@ -1,13 +1,14 @@
 <div class="homeBlock">
-	<div class="content" id="portContent">
+	<div class="content">
 		<div style="opacity:0;height:0;"><h2>ok</h2></div>
 		<div class="center">
 		<h1>Make your very own custom profile here</h1>
-		<p>To add to your profile, please open the console above and use 'addsite -http://SITENAME.com' to enter sites to your portfolio,</p>
+		<p>To add to your profile, please open the console above and use 'addsite -http://SITENAME.com' to enter sites to your portfolio.<br/><br/></p>
 		</div>
-		<div id="portContent" style="width=100%;max-width:1200px;margin-left:auto;margin-right:auto;">
+		<div id="portContent" class="center">
 			<div id="projects">
 			</div>
 		</div>
+	
 	</div>
 </div>
